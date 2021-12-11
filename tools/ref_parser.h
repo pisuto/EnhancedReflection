952f9c76e316725ef31bb7404a9cd0bd60e0c441
pisuto <hiWu501@gmail.com>
2021-12-12 00:04:26 +0800

    add reading .xml file function and remove .cpp

--- tools/ref_parser.h
+++ tools/ref_parser.h
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <fstream>
 #include <stack>
+#include <list>
 #include <functional>
+#include <regex>
 
 namespace ref {
 
@@ -38,113 +40,6 @@ namespace ref {
 
 		std::string file_name;
 		bool is_read, is_wrote;
-	};
-
-	/* xml文件读入 */
-	class xml_parser : public file_parser {
-	public:
-		xml_parser(std::string name) : file_parser(name) {}
-
-		virtual void write_class_name(std::string class_name, int level, int size = -1) override {
-			if (check_if_array(class_name, [&]() {
-				if (size == 0) {
-					out << "</" << end_arr_names.top() << ">";
-					end_arr_names.pop();
-				}
-				else {
-					out << std::endl;
-				} })) {
-				return;
-			}
-			out << "<" << class_name << ">" << std::endl;
-		}
-
-		virtual void write_member_name(std::string var_name, std::string class_name, int level) override {
-			out << std::string(4 * (level), ' ') << "<" << var_name << ">";
-			end_var_name = var_name;
-			if (class_name.find('<') != std::string::npos && class_name.find('>') != std::string::npos) {
-				end_arr_names.push(end_var_name);
-			}
-		}
-
-		virtual void write_ident_space(int level) override {
-			out << std::string(4 * (level), ' ');
-		}
-
-		virtual void write_enter() override {
-			out << std::endl;
-		}
-
-		virtual void write_class_end(std::string class_name) override {
-			if (check_if_array(class_name, [&]() {
-				out << "</" << end_arr_names.top() << ">";
-				end_arr_names.pop();
-			})) {
-				return;
-			}
-			out << "</" << class_name << ">";
-		}
-
-		virtual void write_member_value(std::string type_name, int type_value) {
-			write_member_value_for_base(type_name, type_value);
-		}
-
-		virtual void write_member_value(std::string type_name, std::string type_value) {
-			write_member_value_for_base(type_name, type_value);
-		}
-
-		virtual void write_member_value(std::string type_name, bool type_value) {
-			write_member_value_for_base(type_name, type_value);
-		}
-
-		template<typename T>
-		void write_member_value_for_base(std::string type_name, T type_value) {
-			out << std::boolalpha << type_value << "</" << end_var_name << ">";
-			end_var_name.clear();
-		}
-
-		virtual void read_file() override;
-
-		virtual void write_file() override {
-			// dic.clear(); /* 每一次读入时就清空，需要重新读入 */
-			out.open(file_name, std::ios::out);
-			out << "<?xml version=\"1.0\" encoding=\"utf - 8\" ?>" << std::endl;
-			is_wrote = true;
-			is_read = false;
-		}
-
-		virtual void write_file_finished() override {
-			out.close();
-			is_wrote = false;
-			if (check_if_valid_xml_finished()) {
-				std::cout << "ERROR: Output .xml file has formal error!" << std::endl;
-			}
-		}
-
-		mapping& operator[](std::string index) {
-			static mapping null_map = { "",0,false };
-			return null_map; /* 需要返回一个值 */
-		}
-
-	private:
-		bool check_if_array(std::string class_name, std::function<void()> print) {
-			if (class_name.find('<') != std::string::npos && 
-				class_name.find('>') != std::string::npos) {
-				print();
-				return true;
-			}
-			return false;
-		}
-
-		bool check_if_valid_xml_finished() {
-			return end_arr_names.empty() && end_var_name.empty();
-		}
-
-		std::ifstream in;
-		std::ofstream out;
-
-		std::string end_var_name;
-		std::stack<std::string> end_arr_names;
 	};
 
 	/* .ini .txt文件读入 */
@@ -198,7 +93,69 @@ namespace ref {
 			out << type_name << "{" << std::boolalpha << type_value << "}";
 		}
 
-		virtual void read_file() override;
+		virtual void read_file() override {
+			in.open(file_name, std::ios::in);
+			std::string line;
+			int level = 0;
+			while (std::getline(in, line))
+			{
+				std::string name;
+				{
+					auto pos1 = line.find_first_not_of(" ");
+					auto pos2 = line.find_first_of(" ", pos1);
+					name = line.substr(pos1, pos2 - pos1);
+				}
+
+				std::string value;
+				{
+					auto pos1 = line.find_first_of("{");
+					auto pos2 = line.find_first_of("}");
+					if (pos1 != std::string::npos && pos2 != std::string::npos)
+					{
+						value = line.substr(pos1 + 1, pos2 - pos1 - 1);
+						auto pos1 = value.find_first_not_of(" ");
+						auto pos2 = value.find_last_not_of(" ");
+						if (pos1 == std::string::npos && pos2 == std::string::npos)
+						{
+							pos1 = line.find_first_of("[");
+							pos2 = line.find_first_of("]");
+							if (pos1 != std::string::npos && pos2 != std::string::npos)
+							{
+								value = line.substr(pos1 + 1, pos2 - pos1 - 1);
+								dic.emplace_back(std::make_pair(name, mapping{ value, level, false }));
+							}
+							else
+							{
+								dic.emplace_back(std::make_pair(name, mapping{ "", level, false }));
+							}
+						}
+						else
+						{
+							value = value.substr(pos1, pos2 - pos1 + 1);
+							dic.emplace_back(std::make_pair(name, mapping{ value, level, false }));
+						}
+					}
+					else if (pos2 == std::string::npos)
+					{
+						pos1 = line.find_first_of("[");
+						pos2 = line.find_first_of("]");
+						if (pos1 != std::string::npos && pos2 != std::string::npos)
+						{
+							value = line.substr(pos1 + 1, pos2 - pos1 - 1);
+							dic.emplace_back(std::make_pair(name, mapping{ value, level, false }));
+						}
+						else
+						{
+							dic.emplace_back(std::make_pair(name, mapping{ "", level, false }));
+						}
+						++level;
+					}
+				}
+			}
+			dic.emplace_back(std::make_pair("null", mapping{ "", 0, false }));
+			in.close();
+			is_read = true;
+		}
 
 		virtual void write_file() override {
 			dic.clear(); /* 每一次读入时就清空，需要重新读入 */
@@ -295,5 +252,291 @@ namespace ref {
 			static mapping null_map = { "",0,false };
 			return null_map; /* 需要返回一个值 */
 		}
+	};
+
+	/* xml结构体，不存储attr */
+	enum class XML_STRU_TYPE {
+		/* 错误码 */
+		XML_ERROR_NO_RIGHT_ARROR,
+		XML_ERROR_WRONG_NODE_NAME,
+		XML_ERROR_UNKNOWN,
+
+		/* 节点值 */
+		XML_NULL,		/* 空节点 */
+		XML_DOC,		/* 初始节点 */
+		XML_HEAD,		/* 头节点，'<node>' */
+		XML_TAIL,		/* 尾节点，'</node>' */
+		XML_NODE,       /* 中间节点，'<node></node>' */
+		XML_PCDATA,		/* 数据，'<node>text</node>' */
+		XML_CDATA,		/* <![CDATA[text]]> */
+		XML_COMMENT,	/* '<!-- text -->' */
+		XML_DECL,		/* '<?xml version="1.0"?>' */
+	};
+
+	/* level在xml中由最底层开始递增 */
+	struct xml_text {
+		xml_text() : name(""), value{ "", 0, false } {}
+		std::string name;
+		mapping value;
+	};
+
+	struct xml_node {
+		xml_text text;
+		std::list<xml_node> xml_nodes;
+
+		bool operator==(const xml_node& rhs) {
+			if (text.name == rhs.text.name &&
+				text.value.value == rhs.text.value.value &&
+				text.value.level == rhs.text.value.level &&
+				text.value.checked == rhs.text.value.checked &&
+				xml_nodes.size() == rhs.xml_nodes.size()) {
+				return true;
+			}
+			return false;
+		}
+
+		bool operator!=(const xml_node& rhs) {
+			return !(*this == rhs);
+		}
+	};
+
+	struct xml_doc {
+		std::list<xml_node> xml_nodes;
+	};
+
+	struct xml_tiny_tool {
+
+		void parser_xml(std::string& line, xml_doc& doc) {
+			auto type = get_xml_type(line, doc);
+			switch (type)
+			{
+			case ref::XML_STRU_TYPE::XML_ERROR_NO_RIGHT_ARROR:
+				std::cout << "ERROR: .xml file lacks right arrow!" << std::endl;
+				break;
+			case ref::XML_STRU_TYPE::XML_ERROR_WRONG_NODE_NAME:
+				std::cout << "ERROR: .xml file has error tag!" << std::endl;
+				break;
+			case ref::XML_STRU_TYPE::XML_ERROR_UNKNOWN:
+				std::cout << "ERROR: .xml file encounters unknown error!" << std::endl;
+				break;
+			case ref::XML_STRU_TYPE::XML_NULL:
+			case ref::XML_STRU_TYPE::XML_DOC:
+			case ref::XML_STRU_TYPE::XML_HEAD:
+			case ref::XML_STRU_TYPE::XML_TAIL:
+			case ref::XML_STRU_TYPE::XML_NODE:
+			case ref::XML_STRU_TYPE::XML_PCDATA:
+			case ref::XML_STRU_TYPE::XML_CDATA:
+			case ref::XML_STRU_TYPE::XML_COMMENT:
+			case ref::XML_STRU_TYPE::XML_DECL:
+			default:
+				break;
+			}
+		}
+		
+	private:
+		
+		XML_STRU_TYPE get_xml_type(std::string& line, xml_doc& doc) {
+			if (line.empty()) {
+				return XML_STRU_TYPE::XML_NULL;
+			}
+			xml_node node;
+			auto las /* left arrow start */ = line.find_first_of('<');
+			if (las != std::string::npos) {
+				if (las + 1 < line.length() && line[las + 1] == '?') {
+					return XML_STRU_TYPE::XML_DECL;
+				}
+				auto lae /* left arrow end */ = line.find_first_of('>', las);
+				if (lae != std::string::npos) {
+					if (line[las + 1] != '/') {
+						node.text.name = line.substr(las + 1, lae - las - 1);
+						auto ras /* right arrow start */ = line.find_first_of("</", lae);
+						if (ras != std::string::npos) {
+							auto rae /* right arrow end */ = line.find_first_of('>', ras);
+							if (rae != std::string::npos) {
+								auto node_name = line.substr(ras + 2, rae - ras - 2);
+								if (node_name == node.text.name) {
+									std::string value = lae + 1 != ras? line.substr(lae + 1, ras - lae - 1) : "0";
+									node.text.value = { value, 0, false };
+									xml_store_nodes.top().xml_nodes.push_back(node);
+									return XML_STRU_TYPE::XML_NODE;
+								}
+								return XML_STRU_TYPE::XML_ERROR_WRONG_NODE_NAME;
+							}
+						}
+						else {
+							xml_store_nodes.push(node);
+							return XML_STRU_TYPE::XML_HEAD;
+						}
+					}
+					else {
+						auto temp = xml_store_nodes.top();
+						node.text.name = line.substr(las + 2, lae - las - 2);
+						node.text.value.level = xml_store_nodes.size() - 1;
+						if (temp.text.name == node.text.name) {
+							if (xml_store_nodes.size() == 1) {
+								doc.xml_nodes.push_back(xml_store_nodes.top());
+							}
+							xml_store_nodes.pop();
+							if (!xml_store_nodes.empty()) {
+								auto& top = xml_store_nodes.top();
+								top.xml_nodes.push_back(temp);
+								top.text.value.value = std::to_string(top.xml_nodes.size());
+								top.text.value.level = temp.text.value.level + 1;
+							}
+							return XML_STRU_TYPE::XML_TAIL;
+						}
+						return XML_STRU_TYPE::XML_ERROR_WRONG_NODE_NAME;
+					}
+				}
+				else {
+					return XML_STRU_TYPE::XML_ERROR_NO_RIGHT_ARROR;
+				}
+			}
+			return XML_STRU_TYPE::XML_ERROR_UNKNOWN;
+		}
+
+		std::stack<xml_node> xml_store_nodes;
+	};
+
+	/* xml文件读入 */
+	class xml_parser : public file_parser {
+	public:
+		xml_parser(std::string name) : file_parser(name) {
+			null_node = {};
+			null_node.text.value.level = -1;
+		}
+
+		virtual void write_class_name(std::string class_name, int level, int size = -1) override {
+			if (check_if_array(class_name, [&]() {
+				if (size == 0) {
+					out << "</" << end_arr_names.top() << ">";
+					end_arr_names.pop();
+				}
+				else {
+					out << std::endl;
+				}
+				})) {
+				return;
+			}
+			out << "<" << class_name << ">" << std::endl;
+		}
+
+		virtual void write_member_name(std::string var_name, std::string class_name, int level) override {
+			out << std::string(4 * (level), ' ') << "<" << var_name << ">";
+			end_var_name = var_name;
+			check_if_array(class_name, [&]() {
+				end_arr_names.push(end_var_name);
+				});
+		}
+
+		virtual void write_ident_space(int level) override {
+			out << std::string(4 * (level), ' ');
+		}
+
+		virtual void write_enter() override {
+			out << std::endl;
+		}
+
+		virtual void write_class_end(std::string class_name) override {
+			if (check_if_array(class_name, [&]() {
+				out << "</" << end_arr_names.top() << ">";
+				end_arr_names.pop();
+				})) {
+				return;
+			}
+			out << "</" << class_name << ">";
+		}
+
+		virtual void write_member_value(std::string type_name, int type_value) {
+			write_member_value_for_base(type_name, type_value);
+		}
+
+		virtual void write_member_value(std::string type_name, std::string type_value) {
+			write_member_value_for_base(type_name, type_value);
+		}
+
+		virtual void write_member_value(std::string type_name, bool type_value) {
+			write_member_value_for_base(type_name, type_value);
+		}
+
+		template<typename T>
+		void write_member_value_for_base(std::string type_name, T type_value) {
+			out << std::boolalpha << type_value << "</" << end_var_name << ">";
+			end_var_name.clear();
+		}
+
+		virtual void read_file() override {
+			in.open(file_name, std::ios::in);
+			std::string line;
+			xml_tiny_tool tool;
+			while (std::getline(in, line)) {
+				tool.parser_xml(line, xml_file);
+			}
+			in.close();
+			is_read = true;
+			return;
+		}
+
+		virtual void write_file() override {
+			// dic.clear(); /* 每一次读入时就清空，需要重新读入 */
+			out.open(file_name, std::ios::out);
+			out << "<?xml version=\"1.0\" encoding=\"utf-8\"?>" << std::endl;
+			is_wrote = true;
+			is_read = false;
+		}
+
+		virtual void write_file_finished() override {
+			out.close();
+			is_wrote = false;
+			if (check_if_valid_xml_finished()) {
+				std::cout << "ERROR: Output .xml file has formal error!" << std::endl;
+			}
+		}
+
+		mapping& operator[](std::string index) {
+			for (auto& node : xml_file.xml_nodes) {
+				auto& result = check_if_unchecked_node(index, node);
+				if (result != null_node) {
+					return result.text.value;
+				}
+			}
+			return null_node.text.value;
+		}
+
+	private:
+		bool check_if_array(std::string class_name, std::function<void()> print) {
+			if (class_name.find('<') != std::string::npos &&
+				class_name.find('>') != std::string::npos) {
+				print();
+				return true;
+			}
+			return false;
+		}
+
+		bool check_if_valid_xml_finished() {
+			return end_arr_names.empty() && end_var_name.empty();
+		}
+
+		xml_node& check_if_unchecked_node(std::string& index, xml_node& node) {
+			if (!node.text.value.checked && !node.text.value.value.empty() && index == node.text.name) {
+				return node;
+			}
+			for (auto& sub : node.xml_nodes) {
+				auto& result = check_if_unchecked_node(index, sub);
+				if (result != null_node) {
+					return result;
+				}
+			}
+			return null_node;
+		}
+
+		std::ifstream in;
+		std::ofstream out;
+		xml_doc xml_file;
+
+		std::string end_var_name;
+		std::stack<std::string> end_arr_names;
+
+		xml_node null_node;
 	};
 }
